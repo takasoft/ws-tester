926bf6e5873e1efb79ceaa706531fdea51bb39cf
Takao Shibamoto <takaos@iastate.edu>
2019-12-29 22:55:05 -0600

    init

---
+++ precache-manifest.b50c049029019c8c94a25441f88fd11e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fad11979eb0efd07ca3681586e69de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11551519ab3baf89cf09",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "27eaa75ebfe42adee4a6",
+    "url": "/static/css/main.31b999a4.chunk.css"
+  },
+  {
+    "revision": "11551519ab3baf89cf09",
+    "url": "/static/js/2.7b6a5c3f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.7b6a5c3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27eaa75ebfe42adee4a6",
+    "url": "/static/js/main.cd5883c4.chunk.js"
+  },
+  {
+    "revision": "b3eb52a25e928a525b53",
+    "url": "/static/js/runtime-main.e0e46496.js"
+  }
+]);

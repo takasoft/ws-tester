c55ae10c1e2036e2077a9529311a153ecd925bc4
Takao Shibamoto <takaos@iastate.edu>
2019-12-29 23:02:57 -0600

    update

---
+++ precache-manifest.41371b0fc870d853cdbdba8b3c113bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01300221b65468576e116e7b60250f7b",
+    "url": "./ws-tester/index.html"
+  },
+  {
+    "revision": "11551519ab3baf89cf09",
+    "url": "./ws-tester/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0bedad86105c135b9c0c",
+    "url": "./ws-tester/static/css/main.31b999a4.chunk.css"
+  },
+  {
+    "revision": "11551519ab3baf89cf09",
+    "url": "./ws-tester/static/js/2.7b6a5c3f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./ws-tester/static/js/2.7b6a5c3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bedad86105c135b9c0c",
+    "url": "./ws-tester/static/js/main.1e111bfa.chunk.js"
+  },
+  {
+    "revision": "39aadacde036c9147437",
+    "url": "./ws-tester/static/js/runtime-main.310fb195.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b50c049029019c8c94a25441f88fd11e.js"
+  "./ws-tester/precache-manifest.41371b0fc870d853cdbdba8b3c113bf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./ws-tester/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

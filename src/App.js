934e7c32c0dfeb8dad45c09a9d934e3e5bc25a53
Takao Shibamoto <takaos@iastate.edu>
2019-12-29 22:39:06 -0600

    make it working minimum

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
 
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-java";
@@ -14,6 +16,7 @@ import "ace-builds/src-noconflict/theme-github";
 import ReactJson from 'react-json-view';
 
 import './App.css';
+import Navbar from './Navbar';
 
 function getTime() {
   const today = new Date();
@@ -25,8 +28,8 @@ class App extends React.Component {
     ws: null,
     url: "wss://echo.websocket.org",
     msg: '',
-    history: [
-    ]
+    history: [],
+    connected: false
   }
   constructor(props) {
     super(props);
@@ -37,14 +40,48 @@ class App extends React.Component {
   }
 
   connect() {
+
     const ws = new WebSocket(this.state.url); 
     this.setState({ws});
 
+    this.setState({history: [
+      ...this.state.history,
+      {
+        direction: 'INFO',
+        time: getTime(),
+        type: 'str',
+        data: 'Connecting to ' + this.state.url
+      }
+    ]});
+
     const that = this;
+
+    ws.onerror = (e) => {
+      that.setState({history: [
+        ...that.state.history,
+        {
+          direction: 'ERROR',
+          time: getTime(),
+          type: 'str',
+          data: e.data
+        }
+      ]});
+    }
+
+    ws.onclose = (e) => {
+      that.setState({history: [
+        ...that.state.history,
+        {
+          direction: 'CLOSE',
+          time: getTime(),
+          type: 'str',
+          data: 'code=' + e.code
+        }
+      ]});
+    }
             
-    
     ws.onopen = () => {
-      console.log('CONNECTED: ' + this.state.url)
+      console.log('CONNECTED: ' + that.state.url)
 
       that.setState({history: [
         ...that.state.history,
@@ -52,13 +89,13 @@ class App extends React.Component {
           direction: 'INFO',
           time: getTime(),
           type: 'str',
-          data: 'Connected to ' + this.state.url
+          data: 'Connected to ' + that.state.url
         }
-      ]})
+      ], connected: true});
     };
             
     
-    ws.onmessage = function (e) {
+    ws.onmessage = (e) => {
 
       console.log("RECIEVED: "+ e.data);
       let data = e.data;
@@ -95,86 +132,92 @@ class App extends React.Component {
 
   render() {
     return (
-      <Container>
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="URL"
-            aria-label="URL"
-            aria-describedby="url"
-            value={this.state.url}
-            onChange={(evt) => this.handleUrlChange(evt.target.value)}
-          />
-          <InputGroup.Append>
+      <React.Fragment>
+        <Navbar />
+        <Container>
+          <Row>
+            <InputGroup size="sm">
+              <FormControl
+                placeholder="URL"
+                aria-label="URL"
+                aria-describedby="url"
+                value={this.state.url}
+                onChange={(evt) => this.handleUrlChange(evt.target.value)}
+              />
+              <InputGroup.Append>
+                <Button 
+                  variant="primary" 
+                  onClick={() => this.connect()}
+                >
+                  {this.state.connected ? 'Disconnect' : 'Connect'}
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+          
+          <Row>
+          <div className="log-table-wrapper">
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>Type</th>
+                  <th>Data</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.history.map((val,idx) => {
+                    return (
+                      <tr>
+                        <td>{val.time}</td> 
+                        <td>{val.direction}</td>
+                        <td>
+                          {val.type == 'str' ? (
+                            <pre>{val.data}</pre>
+                          ) : (
+                            <ReactJson src={val.data} />
+                          )}
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </Table>
+          </div>
+          </Row>
+
+          
+          <Row>
+
+          <div className="msg-div">
+            <AceEditor
+              mode="javascript"
+              theme="github"
+              value={this.state.msg}
+              onChange={(evt) => this.handleMsgChange(evt)}
+              name="UNIQUE_ID_OF_DIV"
+              editorProps={{ $blockScrolling: true }}
+              maxLines={10}
+              minLines={3}
+              width="100%"
+              readOnly={!this.state.connected}
+            />
             <Button 
               variant="primary" 
-              onClick={() => this.connect()}
+              onClick={() => this.send()}
+              disabled={!this.state.connected}
             >
-              Connect
+              Send
             </Button>
-          </InputGroup.Append>
-        </InputGroup>
-        <ListGroup>
-          {
-            this.state.history.map((val,idx) => {
-              return (
-                <ListGroup.Item className="url-input-div">
-                  {/* <AceEditor
-                    mode="json"
-                    theme="github"
-                    value={val}
-                    readOnly={true}
-                    name="UNIQUE_ID_OF_DIV"
-                    editorProps={{ $blockScrolling: true }}
-                  /> */}
-                  {val.time} [{val.direction}] 
-                  {val.type == 'str' ? (
-                    val.data
-                  ) : (
-                    <ReactJson src={val.data} />
-                  )}
-                  
-                </ListGroup.Item>
-              )
-            })
-          }
-        </ListGroup>
-        {/* <div>
-          <TextField
-            label="URL"
-            id="standard-adornment-amount"
-            value={this.state.url}
-            onChange={(evt) => this.handleUrlChange(evt.target.value)}
-          />
-          <Button 
-            variant="contained" 
-            color="primary"
-            onClick={() => this.connect()}
-          >
-            Connect
-          </Button>
-        </div> */}
+          </div>
 
-        <div>
-          <AceEditor
-            mode="json"
-            theme="github"
-            value={this.state.msg}
-            onChange={(evt) => this.handleMsgChange(evt)}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{ $blockScrolling: true }}
-            maxLines={10}
-            minLines={3}
-            width="100%"
-          />
-          <Button 
-            variant="primary" 
-            onClick={() => this.send()}
-            disabled={!this.state.ws}
-          >
-            Send
-          </Button>
-        </div>
-      </Container>
+          </Row>
+
+          
+        </Container>
+      </React.Fragment>
     );
   }
   
